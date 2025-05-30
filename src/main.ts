1558a6a39fa9eb6b83cb0376bfa91f89f0e2af1b
KatherinaSl <106989490+KatherinaSl@users.noreply.github.com>
2025-05-30 18:38:55 +0500

    feat: implement OpenAPI spec in doc folder

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { readFileSync } from 'node:fs';
+import * as yaml from 'js-yaml';
+import * as swaggerUi from 'swagger-ui-express';
+import { join } from 'node:path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
+
+  const projectFolder = process.cwd();
+  const yamlPath = join(projectFolder, 'doc', 'api.yaml');
+  const fileContents = readFileSync(yamlPath, 'utf8');
+  const swaggerDocument = yaml.load(fileContents);
+
+  app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
